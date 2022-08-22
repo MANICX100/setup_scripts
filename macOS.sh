b5910445a5e2c51dc5bd88e4dfcf312dc7af1147
MANICX100 <MANICX100@users.noreply.github.com>
2022-08-22 15:27:48 +0100

    vscode and git

--- macOS.sh
+++ macOS.sh
@@ -4,17 +4,24 @@ brew install bash
 brew install dash
 brew install cleanmymac
 brew install the-unarchiver
+
 brew install mas
 brew install ripgrep
 brew install --cask amethyst
 brew install icanhazshortcut
 brew install background-music
+
 brew install onecast
 brew install rectangle
 brew install iina
 brew install soulver
+
 brew install bat
 brew install fd
+brew install vscodium
+
+brew install git
+brew install sublime-merge
 
 mas install 517065482
 mas install 639210716
