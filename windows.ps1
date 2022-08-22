e2f9b2dd04ada9c2da78637daf7ceff2805c3625
MANICX100 <d.manicx100@gmail.com>
2022-08-22 14:34:10 +0100

    spelling correction

--- windows.ps1
+++ windows.ps1
@@ -26,7 +26,7 @@ scoop install nano
 
 scoop install vscode
 
-#chocolatety
+#chocolatey
 gsudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #windows config
