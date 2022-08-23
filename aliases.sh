cfa25ea75bdf7ab8a5cf1b721014f51b8b7113d9
MANICX100 <MANICX100@users.noreply.github.com>
2022-08-23 18:19:48 +0100

    add yt aliases

--- aliases.sh
+++ aliases.sh
@@ -1,7 +1,10 @@
-clear && unclutter -idle 3.00 -root &
+yt() {
+cd "/home/dkendall/Videos/yt/"
+yt-dlp -f 'bv*[height=360]+ba' --download-archive videos.txt  https://www.youtube.com/playlist?list=PLJElTFmVZU3vW-BIfsI2AmfVDL9PzqFmg
+}
 
 alias audit='sudo lynis --forensics && pip-audit'
-alias rcview='sudo bat "/etc/profile.d/aliases.sh"'
+alias rcview='sudo bat --paging=never --style=plain "/etc/profile.d/aliases.sh"'
 alias cls='clear'
 alias screenshot='gnome-screenshot -a'
 alias rc='sudo nano /etc/profile.d/aliases.sh'
@@ -11,29 +14,29 @@ alias update-grub='sudo grub2-mkconfig -o /etc/grub2.cfg && sudo grub2-mkconfig 
 alias edit-grub='sudo nano /etc/default/grub'
 
 x(){
-	if [ -f "$1" ] ; then
-        	case $1 in
-                	*.tar.bz2)   tar xvjf "$1"	;;
-                	*.tar.gz)	tar xvzf "$1"	;;
-                	*.bz2)   	bzip2 "$1" 	;;
-                	*.tar.xz)	tar xf "$1"  	;;
-                	*.rar)   	unrar x "$1" 	;;
-                	*.gz)    	gunzip "$1"  	;;
-                	*.tar)   	tar xvf "$1" 	;;
-                	*.tbz2)  	tar xvjf "$1"	;;
-                	*.tgz)   	tar xvzf "$1"	;;
-                	*.zip)   	unzip "$1"   	;;
-                	*.Z)     	uncompress "$1"  ;;
-                	*.7z)    	7z x "$1"    	;;
-                	*)       	echo "Unable to extract '$1'" ;;
-        	esac
-	else
-        	echo "'$1' is not a valid file"
-	fi
+    if [ -f "$1" ] ; then
+            case $1 in
+                    *.tar.bz2)   tar xvjf "$1"    ;;
+                    *.tar.gz)    tar xvzf "$1"    ;;
+                    *.bz2)       bzip2 "$1"     ;;
+                    *.tar.xz)    tar xf "$1"      ;;
+                    *.rar)       unrar x "$1"     ;;
+                    *.gz)        gunzip "$1"      ;;
+                    *.tar)       tar xvf "$1"     ;;
+                    *.tbz2)      tar xvjf "$1"    ;;
+                    *.tgz)       tar xvzf "$1"    ;;
+                    *.zip)       unzip "$1"       ;;
+                    *.Z)         uncompress "$1"  ;;
+                    *.7z)        7z x "$1"        ;;
+                    *)           echo "Unable to extract '$1'" ;;
+            esac
+    else
+            echo "'$1' is not a valid file"
+    fi
 }
 
 neofetch > ~/.cache/neofetch
-alias pfetch='bat ~/.cache/neofetch'
+alias pfetch='bat --paging=never --style=plain ~/.cache/neofetch'
 sed -i 's/6500/6900/g' ~/.cache/neofetch
 sed -i 's/6400/6800/g' ~/.cache/neofetch
 sed -i 's/3.400/5.500/g' ~/.cache/neofetch
@@ -45,7 +48,13 @@ alias remove='sudo dnf autoremove'
 alias playtvmpv='mpv /home/dkendall/Videos/TV/Personal'
 alias emptybin='sudo rm -rf ~/.local/share/Trash/*'
 
-deltv () {   rm -rf "/home/dkendall/Videos/TV/Personal"; cd "/home/dkendall/Videos/TV/"; mkdir Personal; cd "$HOME"; }
+deltv () {
+rm -rf "/home/dkendall/Videos/TV/Personal"; cd "/home/dkendall/Videos/TV/"; mkdir Personal; cd "$HOME";
+}
+
+delyt() {
+rm -rf "/home/dkendall/Videos/yt"; cd "/home/dkendall/Videos/"; mkdir yt ; cd "$HOME";
+}
 
 alias delrecent='sudo rm ~/.local/share/recently-used.xbel && sudo touch ~/.local/share/recently-used.xbel'
 
@@ -53,7 +62,7 @@ alias del='sudo rm -rf -v -I'
 
 alias syncfolders='rsync -progress -avh --ignore-existing /home/dkendall/ /run/media/dkendall/exFAT/ --delete --exclude=".*" '
 
-subs() {   "$HOME/OneDrive/OpenSubtitlesDownload.py" --cli --auto --username MANICX100 --password 5z6!!Evd "$1";	}
+subs() {   "$HOME/OneDrive/OpenSubtitlesDownload.py" --cli --auto --username MANICX100 --password 5z6!!Evd "$1";    }
 
 alias unshareusb='/bin/eveusbc unshare all'
 alias shareusb='/bin/eveusbc share 12345 1-9.1'
@@ -78,10 +87,10 @@ alias gohome='cd "$HOME"'
 orderfiles() {
 for filename in *; do
   if [[ -f "$filename" ]]; then
-  	base=${filename%.*}
-  	ext=${filename#$base.}
-	mkdir -p "${ext}"
-	mv "$filename" "${ext}"
+      base=${filename%.*}
+      ext=${filename#$base.}
+    mkdir -p "${ext}"
+    mv "$filename" "${ext}"
   fi
 done
 }
