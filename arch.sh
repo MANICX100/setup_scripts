4103a057c1a99c8e04fc525e3a0d73947f53aff8
MANICX100 <MANICX100@users.noreply.github.com>
2022-08-26 17:15:14 +0100

    Create arch.sh

---
+++ arch.sh
@@ -0,0 +1,72 @@
+echo "Arch setup"
+
+sudo -k echo "Yes"
+
+paru -S mupdf unrar libheif mpv stacer feh ncompress onedrive
+paru -S unclutter
+paru -S i3lock
+paru -S bat
+paru -S lynis
+
+paru -S synergy
+paru -S openssl
+
+paru -S sddm
+
+systemctl disable gdm
+systemctl enable sddm
+
+paru -S mksh
+sudo usermod -s /bin/mksh dkendall
+
+systemctl --user enable onedrive
+systemctl --user start onedrive
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+paru -S snapd
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install snapd
+sudo snap set system refresh.retain=2
+sudo snap install plexmediaserver
+sudo snap install thunderbird --candidate
+sudo snap install savewizard --devmode
+
+echo "Remember to load up software centre and install the following:"
+echo "Flatseal"
+echo "SCITE"
+echo "Video Trimmer"
+echo "Sublime Merge"
+echo "VS Code"
+
+sudo -k echo "Yes"
+
+echo "Download yt-dlp AND topgrade and install to"
+sudo xdg-open "/usr/local/bin"
+
+sudo -k echo "Yes"
+
+echo "Plug in USB HDD and press enter to rsync"
+
+sudo -k echo "Yes"
+
+rsync -progress -avh --ignore-existing /home/dkendall/ /run/media/dkendall/exFAT/ --delete --exclude=".*"
+
+paru -S python3.11
+pip install pip-audit
+
+echo "USB Network Gate"
+echo "Tabby Terminal"
+echo "Java update"
+
+sudo -k echo "Yes"
+
+sudo alternatives --config java
+
+echo ".appimage"
+echo "Chiaki"
+echo "WebCatalog"
+
+sudo -k echo "Yes"
+
+onedrive --synchronize
