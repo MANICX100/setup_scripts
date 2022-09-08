76e824e66b60a219ffb9d958a92d3f640dba58d4
MANICX100 <MANICX100@users.noreply.github.com>
2022-09-08 20:48:04 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,47 +1,36 @@
-# Write-Host "Please temporily disable UAC to avoid password prompts"
-# Read-Host
+Write-Host "Please temporily disable UAC to avoid password prompts"
+Read-Host
 
-# #scoop
-# Invoke-RestMethod get.scoop.sh | Invoke-Expression
-# scoop install git
-# scoop bucket add extras
+#scoop
+Invoke-RestMethod get.scoop.sh | Invoke-Expression
+scoop install git
+scoop bucket add extras
 
-# scoop install gsudo
+scoop install gsudo
 
-# scoop install sumatrapdf
-# scoop install vlc
-# scoop install topgrade
-# scoop install thunderbird
-# scoop install irfanview
+scoop install sumatrapdf
+scoop install vlc
+scoop install topgrade
+scoop install thunderbird
+scoop install irfanview
 
-# scoop install fd
-# scoop install ripgrep
-# scoop install gawk
-# scoop install sed
-# scoop install bat
-# scoop install wget
+scoop install fd
+scoop install ripgrep
+scoop install gawk
+scoop install sed
+scoop install bat
+scoop install wget
 
-# #scoop install tabby
-
-# scoop install notepad3
-
-# scoop install nano
-
-# #scoop install vscode
-
-# #chocolatey
-# gsudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+scoop install tabby
+scoop install notepad3
+scoop install nano
+scoop install vscode
 
 #windows config
 gsudo powercfg.exe /hibernate off
 gsudo reg add HKLM\SYSTEM\ControlSet001\Control\Bluetooth\Audio\AVRCP\CT /v DisableAbsoluteVolume /t REG_DWORD /d 1 /f
 gsudo fsutil behavior set disablelastaccess 1
 gsudo Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 1
-
-#winget
-winget install -e --id Microsoft.DotNet.Runtime.6
-winget install -e --id Microsoft.PowerToys
-#winget install -e --id BitSum.ProcessLasso
 
 #next steps
 Write-Host "Remember to download graphics drivers"
@@ -62,8 +51,14 @@ else {
     gsudo nvcleanstall
 }
 
+Write-Host "Remember to update powershell and add aliases e.g. pwsh"
+Read-Host
+
 Write-Host "Remember to change restore point frequency"
 Read-Host
 
 Write-Host "Remember to configure WSA"
+Read-Host
+
+Write-Host "run topgrade to configure winget choco optional"
 Read-Host
