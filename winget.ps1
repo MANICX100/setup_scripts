66603af5811983b73a2fba502b5c64b00380625c
MANICX100 <MANICX100@users.noreply.github.com>
2022-09-15 17:46:37 +0100

    Create winget.ps1

---
+++ winget.ps1
@@ -0,0 +1,9 @@
+gsudo topgrade
+#Windows Terminal Preview
+winget install 9N8G5RFZ9XK3
+#Twinkle Tray
+winget install xanderfrangos.twinkletray
+#EarTrumpet
+winget install 9NBLGGH516XP
+#Paint.NET
+winget install 9NBHCS1LX4R0
