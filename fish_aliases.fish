c3d363f4dd38fce2f2be5c8fe49447ef2fde657b
MANICX100 <MANICX100@users.noreply.github.com>
2022-09-24 22:39:29 +0100

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -159,3 +159,48 @@ function x
     extract $argv;
     cd "$HOME";
 end
+
+function newgitmsg
+  git add .
+  git commit -a -m "$argv"
+  git push
+end
+
+function newgit
+  git add .
+  git commit -a --allow-empty-message -m " "
+  git push
+end
+
+function gitprep
+	git stash
+	git pull
+	git stash pop
+end
+
+function gitPromoteAndMerge
+	echo "This will attempt to merge an existing branch with master. Any existing in master and not in the branch will be attempted to be preserved"
+	git checkout -b "$argv"
+	git status
+	git commit -a --allow-empty-message -m " "
+	git checkout master
+	git merge --no-ff "$argv"
+	git push origin master
+  end
+  
+function gitDeleteBranch
+	git branch -d "$argv"
+	git push origin --delete "$argv"
+end
+
+function gitRenameBranch
+	echo "Please run the following commands in sequence"
+	echo 'git branch -m "$old" "$new" '
+	echo 'git branch --unset-upstream "$new" '
+	echo 'git push origin "$new" '
+	echo 'git push origin -u "$new" '
+end
+
+function gitIgnoreRm
+	git ls-files -i -c --exclude-from=.gitignore | %{git rm --cached $_}
+end
