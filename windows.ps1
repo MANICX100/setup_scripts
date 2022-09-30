69aa5352b59383371ffff8d71a05279df3da9bfa
MANICX100 <MANICX100@users.noreply.github.com>
2022-09-30 16:54:21 +0100

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -32,6 +32,8 @@ scoop install yt-dlp
 
 #scoop install vscode
 
+scoop install lockhunter
+
 #windows config
 gsudo powercfg.exe /hibernate off
 gsudo reg add HKLM\SYSTEM\ControlSet001\Control\Bluetooth\Audio\AVRCP\CT /v DisableAbsoluteVolume /t REG_DWORD /d 1 /f
