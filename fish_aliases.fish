fd28c1bc50e9c56d073ad1ab549ac4ae66d0e2fa
MANICX100 <MANICX100@users.noreply.github.com>
2022-10-17 18:58:37 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -3,7 +3,8 @@ set fish_greeting
 cd "$HOME"
 
 function image2txt
-	tesseract -l eng $argsv[1] output_from_ocr
+	read -l -P '' confirm
+	tesseract -l eng $confirm output_from_ocr
 	cat output_from_ocr.txt
 end
 
