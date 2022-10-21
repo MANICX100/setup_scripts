1a05450aa1d3eb006e04ad397aa2d4766d290b1d
MANICX100 <MANICX100@users.noreply.github.com>
2022-10-21 10:07:19 +0000

    Update windows-standard.ps1

--- windows-standard.ps1
+++ windows-standard.ps1
@@ -38,12 +38,6 @@ scoop install lockhunter
 scoop install windows-terminal-preview
 scoop install pwsh
 
-#windows config
-gsudo powercfg.exe /hibernate off
-gsudo reg add HKLM\SYSTEM\ControlSet001\Control\Bluetooth\Audio\AVRCP\CT /v DisableAbsoluteVolume /t REG_DWORD /d 1 /f
-gsudo fsutil behavior set disablelastaccess 1
-gsudo Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value 1
-
 #next steps
 Write-Host "Remember to download graphics drivers"
 Write-Host "Press Enter to continue"
