b9c010d4bd4b3854516a72249a5c5df426fc907f
MANICX100 <MANICX100@users.noreply.github.com>
2022-11-09 06:24:12 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -19,7 +19,7 @@ alias ffprefup='/home/dkendall/.mozilla/firefox/oewuk6x8.default-release/updater
 alias ipconfig='ip route'
 alias ifconfig='ip route'
 
-alias stripclip="echo xclip |awk -v OFS=' ' '{$1=$1}1|xclip"
+alias stripclip='echo xclip |awk -v OFS=' ' '{$1=$1}1|xclip'
 
 alias mpv='mpv --ontop --force-window'
 alias audit='sudo lynis --forensics && pip-audit'
