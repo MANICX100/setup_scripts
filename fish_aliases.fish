4db41df5f19608caa4b146d2bbf091a2314367d5
MANICX100 <MANICX100@users.noreply.github.com>
2022-11-10 12:34:04 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -185,31 +185,10 @@ function gitprep
 	git stash pop
 end
 
-function gitPromoteAndMerge
-	echo "This will attempt to merge an existing branch with master. Any existing in master and not in the branch will be attempted to be preserved"
-	git checkout -b "$argv"
-	git status
-	git commit -a --allow-empty-message -m " "
-	git checkout master
-	git merge --no-ff "$argv"
-	git push origin master
-  end
-  
-function gitDeleteBranch
-	git branch -d "$argv"
-	git push origin --delete "$argv"
-end
-
-function gitRenameBranch
-	echo "Please run the following commands in sequence"
-	echo 'git branch -m "$old" "$new" '
-	echo 'git branch --unset-upstream "$new" '
-	echo 'git push origin "$new" '
-	echo 'git push origin -u "$new" '
-end
-
 function gitIgnoreRm
-	git ls-files -i -c --exclude-from=.gitignore | %{git rm --cached $_}
+	git rm -r --cached .
+	git add .
+	git commit -m "Update .gitignore"
 end
 
 starship init fish | source
