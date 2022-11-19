ba71b021d73b1fbbd420d0c71792ce056dc6e10a
MANICX100 <MANICX100@users.noreply.github.com>
2022-11-19 17:08:47 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -2,7 +2,7 @@ set fish_greeting
 set now date -u +%Y-%m-%dT%H:%M:%S%Z
 
 function speedupvid
-	  ffmpeg -i $argv[1] -filter_complex "[0:v]setpts=<1/$argv[2]>*PTS[v];[0:a]atempo=<$argv[2]>[a]" -map "[v]" -map "[a]" output.mkv
+	  ffmpeg -i $argv[1] -filter_complex "[0:v]setpts=1/$argv[2]*PTS[v];[0:a]atempo=$argv[2][a]" -map "[v]" -map "[a]" output.mkv
 end
 
 function image2txt
