c9a76da5f61fe17a50cdb20e66fa79d32d3f4add
MANICX100 <MANICX100@users.noreply.github.com>
2022-11-20 12:25:04 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -1,5 +1,5 @@
 set fish_greeting
-set now date -u +%Y-%m-%dT%H:%M:%S%Z
+set now date -u +%Y-%m-%dT%H-%M-%S%Z
 
 function speedupvid
 	  ffmpeg -i $argv[1] -filter_complex "[0:v]setpts=1/$argv[2]*PTS[v];[0:a]rubberband=tempo=$argv[2][a]" -map "[v]" -map "[a]" $($now)-output.mkv
