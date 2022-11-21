ed1a20ab7bef5ea07eb78ed03bfbaea045ac5f5d
MANICX100 <MANICX100@users.noreply.github.com>
2022-11-21 09:56:20 +0000

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -306,7 +306,11 @@ function openall{
 }
 
 function screenrec{
-ffmpeg -f dshow -i audio="Microphone (5- SteelSeries Arctis 1 Wireless)" -f -y -f gdigrab -framerate 30 -draw_mouse 1 -i desktop -c:v libx264 $(Get-Date -UFormat "%Y-%m-%d_%H-%m-%S")-output.mp4
+ffmpeg -f dshow -i audio="@device_cm_{33D9A762-90C8-11D0-BD43-00A0C911CE86}\wave_{44180CE1-1ECF-4C4D-B8DE-3680832ECB6D}" -f mkv -y -f gdigrab -framerate 30 -draw_mouse 1 -i desktop -c:v libx264 -f mp4 $(Get-Date -UFormat "%Y-%m-%d_%H-%m-%S").mkv
+}
+
+function ffmpeglist {
+ffmpeg -list_devices true -f dshow -i dummy
 }
 
 function gohome{
