29e3b30f4539fd764dbc37e4410893d851da6a0e
MANICX100 <MANICX100@users.noreply.github.com>
2022-12-16 17:19:33 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -3,13 +3,13 @@ set now date -u +%Y-%m-%dT%H-%M-%S%Z
 
 function burnin-srt
 	set filename basename "$argv[1]"
-	set base filename%.*
+	set base $filename%.*
 	ffmpeg -i "$argv[1]" -vf subtitles="$argv[2]" -preset ultrafast "$base-srt.mkv"
 end
 
 function speedupvid
 	set filename basename "$argv[1]"
-	set base filename%.*
+	set base $filename%.*
 	ffmpeg -i "$argv[1]" -filter_complex "[0:v]setpts=1/$argv[2]*PTS[v];[0:a]rubberband=tempo=$argv[2][a]" -map "[v]" -map "[a]" -preset ultrafast "$base-speed.mkv"
 end
 
