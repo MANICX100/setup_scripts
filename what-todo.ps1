d2a0329d850ce16bd72cea26d4135fc9786056e5
MANICX100 <MANICX100@users.noreply.github.com>
2022-12-20 15:40:17 +0000

    Update what-todo.ps1

--- what-todo.ps1
+++ what-todo.ps1
@@ -7,8 +7,8 @@ echo ""
 echo "If this failed please reboot and run again"
 echo ""
 
-mkdir Documents/WindowsPowershell
-mkdir Documents/Powershell
+mkdir -p Documents/WindowsPowershell
+mkdir -p Documents/Powershell
 
 irm "https://onedrive.live.com/download?cid=77AE4ECB7EF00365&resid=77AE4ECB7EF00365%2125801&authkey=AMz08_VLHfC5zAo" -outFile what-todo.zip
 Expand-Archive what-todo.zip
@@ -16,4 +16,4 @@ Expand-Archive what-todo.zip
 cd what-todo
 pnpm i
 
-wget --no-cache -O $profile "https://github.com/MANICX100/setup_scripts/raw/main/what-todo-rc.ps1"
+irm "https://github.com/MANICX100/setup_scripts/raw/main/what-todo-rc.ps1" -outFile $profile
