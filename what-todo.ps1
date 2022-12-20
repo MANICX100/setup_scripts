a4d45bdac0c6360aed7687589a771e0e965d25f1
MANICX100 <MANICX100@users.noreply.github.com>
2022-12-20 10:13:24 +0000

    Update what-todo.ps1

--- what-todo.ps1
+++ what-todo.ps1
@@ -1,5 +1,5 @@
 Remove-Item -recurse what-todo -Force
 irm "https://onedrive.live.com/download?cid=77AE4ECB7EF00365&resid=77AE4ECB7EF00365%2125801&authkey=AMz08_VLHfC5zAo" -outFile what-todo.zip
-Extract-Archive what-todo.zip
+Expand-Archive what-todo.zip
 
 wget --no-cache -O $profile "https://github.com/MANICX100/setup_scripts/raw/main/what-todo-rc.ps1"
