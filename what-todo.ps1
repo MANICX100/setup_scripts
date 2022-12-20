93e796e5c476023af9482af56853500ccb3a132a
MANICX100 <MANICX100@users.noreply.github.com>
2022-12-20 15:35:20 +0000

    Update what-todo.ps1

--- what-todo.ps1
+++ what-todo.ps1
@@ -3,7 +3,12 @@ scoop install pnpm
 pnpm setup
 pnpm env use latest --global
 
+echo ""
 echo "If this failed please reboot and run again"
+echo ""
+
+mkdir Documents/WindowsPowershell
+mkdir Documents/Powershell
 
 irm "https://onedrive.live.com/download?cid=77AE4ECB7EF00365&resid=77AE4ECB7EF00365%2125801&authkey=AMz08_VLHfC5zAo" -outFile what-todo.zip
 Expand-Archive what-todo.zip
