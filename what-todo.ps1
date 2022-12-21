c45f9d07694a4bf734ed2f0dbf207fc225bda3c9
MANICX100 <MANICX100@users.noreply.github.com>
2022-12-21 14:11:48 +0000

    Update what-todo.ps1

--- what-todo.ps1
+++ what-todo.ps1
@@ -10,10 +10,10 @@ echo ""
 mkdir -p Documents/WindowsPowershell
 mkdir -p Documents/Powershell
 
-irm "https://onedrive.live.com/download?cid=77AE4ECB7EF00365&resid=77AE4ECB7EF00365%2125801&authkey=AMz08_VLHfC5zAo" -outFile what-todo.zip
-Expand-Archive what-todo.zip
+irm "https://onedrive.live.com/download?cid=77AE4ECB7EF00365&resid=77AE4ECB7EF00365%2126017&authkey=AGN3pMtZt8SoRXE" -outFile what-todo.zip
+Expand-Archive whatTodoUI.zip
 
-cd what-todo
+cd whatTodoUI
 pnpm i
 
 irm "https://github.com/MANICX100/setup_scripts/raw/main/what-todo-rc.ps1" -outFile $profile
