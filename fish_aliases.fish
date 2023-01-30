f6dbb56ba2a42527d4325bdd3fc4dac448ac4c0f
MANICX100 <MANICX100@users.noreply.github.com>
2023-01-30 12:20:50 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -23,14 +23,14 @@ alias Get-Volume='sudo lsblk -o NAME,FSTYPE,SIZE,MOUNTPOINT,LABEL'
 function burnin-srt
 	set filename (basename "$argv[1]")  # get the full file name
 	set base (echo $filename | sed 's/\.[^.]*$//')  # get the file name without the extension
-	ffmpeg -i "$argv[1]" -vf subtitles="$argv[2]" -preset ultrafast "$base-srt.mkv"
+	ffmpeg -i "$argv[1]" -vf subtitles="$argv[2]" -c:v libopenh264 -preset ultrafast -threads 0 "$base-srt.mkv"
 end
 
 function speedupvid
 	set filename (basename "$argv[1]")  # get the full file name
 	set extension (echo $filename | sed 's/^.*\.//')  # get the extension
 	set base (echo $filename | sed 's/\.[^.]*$//')  # get the file name without the extension
-	ffmpeg -i "$argv[1]" -filter_complex "[0:v]setpts=1/$argv[2]*PTS[v];[0:a]rubberband=tempo=$argv[2][a]" -map "[v]" -map "[a]" -c:v libopenh264 -preset ultrafast -threads 1 "$base-speed.mkv"
+	ffmpeg -i "$argv[1]" -filter_complex "[0:v]setpts=1/$argv[2]*PTS[v];[0:a]rubberband=tempo=$argv[2][a]" -map "[v]" -map "[a]" -c:v libopenh264 -preset ultrafast -threads 0 "$base-speed.mkv"
 end
 
 alias apptime='timeout --signal INT 1s time $argv'
