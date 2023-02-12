909528088518d11d53280adfc52c69a9305f32d4
MANICX100 <MANICX100@users.noreply.github.com>
2023-02-12 19:35:30 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -209,7 +209,7 @@ alias delrecent='sudo rm ~/.local/share/recently-used.xbel && sudo touch ~/.loca
 
 alias rm='rm -rf -v'
 
-alias syncfolders='rsync -avh --ignore-existing --exclude ".*" --delete --progress /home/dkendall/ /run/media/dkendall/exFAT/'
+alias syncfolders='rsync -avh --ignore-existing --exclude ".*" --delete --progress --compress --no-whole-file /home/dkendall/ /run/media/dkendall/exFAT/'
 
 alias unshareusb='/bin/eveusbc unshare all'
 alias shareusb='/bin/eveusbc share 12345 1-9.1'
