11cb6f23eb614f4926ec5b61b78ed67198d1d1d0
MANICX100 <MANICX100@users.noreply.github.com>
2023-02-24 14:44:25 +0000

    Update fish_aliases.fish

--- fish_aliases.fish
+++ fish_aliases.fish
@@ -1,6 +1,9 @@
 set fish_greeting
 set now date -u +%Y-%m-%dT%H-%M-%S%Z
 
+alias lr='zypper lr'
+alias ffmpeg='/usr/local/bin/ffmpeg'
+
 alias linuxservices='systemctl list-unit-files --type=service --state=enabled'
 alias macosservices='sudo launchctl list'
 
