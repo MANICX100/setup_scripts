d0b2d1c11f9386ef89214c7dd79e66acdd6e4cb4
MANICX100 <MANICX100@users.noreply.github.com>
2023-03-12 13:46:00 +0000

    Update fedora.sh

---
+++ linux-standard.sh
@@ -0,0 +1,5 @@
+mupdf unrar libheif mpv stacer feh ncompress onedrive unclutter i3lock bat lynis openssl sddm gnome-screenshot fish util-linux-user python3.11 ffmpeg gh gnome-tweak-tool qemu bash coreutils edk2-tools grep jq lsb procps python3 genisoimage usbutils util-linux sed spice-gtk-tools swtpm wget xdg-user-dirs xrandr unzip aria2
+
+curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz
+chmod +x gdu_linux_amd64
+sudo mv gdu_linux_amd64 /usr/bin/gdu
