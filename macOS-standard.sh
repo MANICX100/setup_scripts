53562da5189aa2fc46fa3806a11a1deae7b538e1
MANICX100 <MANICX100@users.noreply.github.com>
2023-03-12 13:47:19 +0000

    Update macOS.sh

---
+++ macOS-standard.sh
@@ -0,0 +1,15 @@
+topgrade zsh bash fish fisher cleanmymac the-unarchiver ncompress gzip bzip2 mas ripgrep amethyst icanhazshortcut background-music onecast rectangle iina soulver bat fd steam git gh paragon-ntfs paragon-extfs gdu gnu-time parallels
+
+brew link --overwrite gdu
+
+#hashtab
+mas install 517065482
+
+#neooffice
+mas install 639210716
+
+#onedrive
+mas install 823766827
+
+chsh -s /usr/local/bin/fish
+sudo chsh -s /usr/local/bin/fish
