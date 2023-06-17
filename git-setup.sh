c279d39effd1e10c11f79d9a7cd73fac325efbb4
Danny Kendall <MANICX100@users.noreply.github.com>
2023-06-17 22:27:47 +0000

    Update git-setup.sh

--- git-setup.sh
+++ git-setup.sh
@@ -1,11 +1,8 @@
-git config --global user.name "MANICX100"
+git config --global user.name "Danny Kendall"
 git config --global user.email "d.manicx100@gmail.com"
-
-#git config --global user.name "Danny Kendall"
-#git config --global user.email "daniel.kendall@ideagenplc.com"
-
-#gsudo git config --global http.sslcainfo "%USERPROFILE%\scoop\apps\git\current\mingw64\ssl\certs\ca-bundle.crt"
 git config --global rebase.updateRefs true
 git config --global credential.helper store
 git config --global --add push.default current
 git config --global --add push.autoSetupRemote true
+
+#gsudo git config --global http.sslcainfo "%USERPROFILE%\scoop\apps\git\current\mingw64\ssl\certs\ca-bundle.crt"
