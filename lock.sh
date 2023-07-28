856f9c3965973bc63fa84879dd9ec6f86cc27197
Danny Kendall <MANICX100@users.noreply.github.com>
2023-07-28 21:55:22 +0000

    Update lock.sh

--- lock.sh
+++ lock.sh
@@ -1,10 +1,11 @@
-# Launch Contour 
+# Launch Contour and save PID
+contour_pid=$!
 flatpak run org.contourterminal.Contour &
 
-# Wait briefly for it to launch 
+# Wait briefly for it to launch
 sleep 1
 
-# Send Ctrl+D to exit Contour
-killall contour
+# Exit Contour instance we launched 
+kill $contour_pid
 
 xset dpms force off && i3lock -c 000000 -n
