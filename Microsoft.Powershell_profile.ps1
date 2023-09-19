21e4fb9312b7205bbba30e9b020c2dfacd8b3d7c
Danny Kendall <MANICX100@users.noreply.github.com>
2023-09-19 11:02:40 +0100

    Update Microsoft.Powershell_profile.ps1

--- Microsoft.Powershell_profile.ps1
+++ Microsoft.Powershell_profile.ps1
@@ -6,7 +6,8 @@ Set-Alias afconvert "ffmpeg"
 Set-Alias afinfo "mediainfo"
 Set-Alias afplay "mpv"
 Set-Alias airport "networkstatus"
-Set-Alias alloc "free"
+Set-Alias alloc "wmic OS get FreePhysicalMemory /Value"
+Set-Alias free "wmic OS get FreePhysicalMemory /Value"
 Set-Alias apropos "tldr"
 Set-Alias -Name ffprobe -Value mediainfo
 Set-Alias -Name trash -Value Remove-ItemSafely
