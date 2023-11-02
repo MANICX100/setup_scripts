7f8be7f62bc7552ccb46e58d10359686e7bf4a07
Danny Kendall <MANICX100@users.noreply.github.com>
2023-11-02 07:51:03 +0000

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if mount | rg -q "/dev/nvme1n1p4 on /media/dkendall/windows"; then
+if mount | rg -q "/dev/nvme1n1p5 on /media/dkendall/windows"; then
   xfe "/media/dkendall/windows/Users/dkendall/"
 else
-  lxqt-sudo mount /dev/nvme1n1p4 /media/dkendall/windows
+  lxqt-sudo mount /dev/nvme1n1p5 /media/dkendall/windows
   xfe "/media/dkendall/windows/Users/dkendall/"
 fi
