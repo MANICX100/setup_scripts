5d16276284ede905c1d6b6b2798468f1ce39def1
Danny Kendall <MANICX100@users.noreply.github.com>
2024-05-22 22:47:53 +0100

    Create cyclewallpapers.sh

---
+++ cyclewallpapers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Directory containing wallpapers
+WALLPAPER_DIR="/home/dkendall/OneDrive/Wallpapers"
+
+# Interval in seconds between wallpaper changes
+INTERVAL=300  # Change every 5 minutes
+
+while true; do
+    for wallpaper in "$WALLPAPER_DIR"/*; do
+        # Set the wallpaper with feh using --bg-fill
+        feh --bg-fill "$wallpaper"
+        # Wait for the specified interval
+        sleep "$INTERVAL"
+    done
+done
